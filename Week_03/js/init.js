d543b91da659f935aaf9504d835890eccea562f7
Jess Xu <jsxu315@gmail.com>
2021-04-22 15:40:40 -0700

    Added the right files this time

--- Week_03/js/init.js
+++ Week_03/js/init.js
@@ -8,50 +8,43 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// adding markers
-let work = L.marker([34.0709, -118.444]).addTo(map)
-		.bindPopup('Where I work on campus')
-
-let home = L.marker([37.7409, -122.484]).addTo(map)
-		.bindPopup('Where I currently am')
-
-let random = L.marker([39.7409, -122.484]).addTo(map)
-		.bindPopup('Third Point')
-
 
 let nebraska = [40.927316893065885, -99.95246387738227];
-addMarker(nebraska[0], nebraska[1], "This is Nebraska", "Fly to Nebraska"); 
+addExcitedMarker(nebraska[0], nebraska[1], "This is Nebraska", 1); 
 
-addExcitedMarker(37.554161894514564, -122.09429257186252, "The Bay Area", 13); 
-addExcitedMarker(45.067025356140185, -86.12093542264297, "The Great Lakes", 1); 
-addExcitedMarker(50.57739648211621, -75.31038942567739, "Canada", 3); 
+addExcitedMarker(37.554161894514564, -122.09429257186252, "The Bay Area (Point Reyes)", 13); 
+addExcitedMarker(45.067025356140185, -86.12093542264297, "The Great Lakes", 5); 
+addExcitedMarker(50.57739648211621, -75.31038942567739, "Niagara Falls and maybe Toronto", 7); 
 
 
 function addExcitedMarker(lat, long, message, howExcited) 
 //set isExcited to true to add exclamation points
 {
+    createButton(lat, long, message); 
+
     let excite = "!";
     for(let i = 0; i < howExcited; i++)
     {
         excite += "!"; 
     }
-
     message += excite; 
     L.marker([lat, long]).addTo(map).bindPopup(message);
+
     return message; 
+
 }
 
-function addMarker(lat, long, message, title) 
-{
-    console.log(message); 
-    L.marker([lat, long]).addTo(map).bindPopup(message);
-    createButton(lat, long, title);
-    return message;  
-}
+// function addMarker(lat, long, message, title) 
+// {
+//     console.log(message); 
+//     L.marker([lat, long]).addTo(map).bindPopup(message);
+//     createButton(lat, long, title);
+//     return message;  
+// }
 
 function createButton(lat, long, title)
 {
-    const newButton = document.createElement("button"); //is this from a leaflet class or a common library? 
+    const newButton = document.createElement("button");
     newButton.id = "button" + title; 
     newButton.innerHTML = title; 
     newButton.setAttribute("lat", lat);
